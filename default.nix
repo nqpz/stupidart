0e8e235b94b2e0fb17715ad966d2eeb19f94168a
Niels G. W. Serup <ngws@metanohi.name>
2020-11-02 20:48:55 +0100

    Make more Nix-friendly

--- default.nix
+++
@@ -1,7 +0,0 @@
-# You can run nix-shell in this directory if you have Nix installed.
-
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-    name = "stupidart";
-    buildInputs = [ pkgconfig SDL2 SDL2_ttf freeimage ocl-icd opencl-headers ];
-}
